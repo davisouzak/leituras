c81a9fb94a461492a662b4f22c52e2fca9520c93
davisouzak <juliejonatasaraujo@gmail.com>
2025-05-08 12:24:15 -0300

    finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CabecalhoComponent, RodapeComponent, ListaLivrosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'organo';
